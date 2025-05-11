4bfef336e77f8940d93fa786d7299a9bea9bb63e
Alok Ahirrao <104273243+alok-ahirrao@users.noreply.github.com>
2025-05-11 13:07:02 +0530

    Python3 Solution
          Submission Statistics:
          Question #: 1293
          Language: Python3
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 17.832 MB
          Memory percentile: 61.69

--- 1293. Three Consecutive Odds/Solution.py
+++ 1293. Three Consecutive Odds/Solution.py
@@ -1,4 +1,11 @@
 class Solution:
-    def threeConsecutiveOdds(self, a: List[int]) -> bool:
-        return False if len(a)<3 else any(1&a[i]&a[i+1]&a[i+2] for i in range(len(a)-2))
-        
+    def threeConsecutiveOdds(self, arr: List[int]) -> bool:
+        crt = 0
+        for i in arr:
+            if i%2!=0:
+                crt +=1
+                if crt >=3:
+                    return True
+            else:
+                crt = 0 
+        return False 
