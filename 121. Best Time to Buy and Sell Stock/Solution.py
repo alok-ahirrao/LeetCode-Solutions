f8fcb0f8adfb487a462ea9ee3abac72a8a6ac596
Alok Ahirrao <104273243+alok-ahirrao@users.noreply.github.com>
2025-05-18 13:02:33 +0530

    Python3 Solution
          Submission Statistics:
          Question #: 121
          Language: Python3
          Runtime: 105 ms
          Runtime percentile: 42.83
          Memory: 26.94 MB
          Memory percentile: 33.23

--- 121. Best Time to Buy and Sell Stock/Solution.py
+++ 121. Best Time to Buy and Sell Stock/Solution.py
@@ -4,13 +4,11 @@ class Solution:
         r,l=1,0
         maxProfit = 0
         while r<len(prices):
-            profit=0
             if prices[l] <prices[r]:
                 profit = prices[r] -prices[l]
+                maxProfit=max(maxProfit,profit)
+
             else:
                 l=r
-            maxProfit=max(maxProfit,profit)
             r+=1
         return maxProfit
-            
-
