3ff0674030af2b2dbacb5565f71724e1be7f9425
Alok Ahirrao <104273243+alok-ahirrao@users.noreply.github.com>
2025-05-28 00:14:44 +0530

    Python3 Solution
          Submission Statistics:
          Question #: 3172
          Language: Python3
          Runtime: 4 ms
          Runtime percentile: 28.45
          Memory: 17.844 MB
          Memory percentile: 39.75

--- 3172. Divisible and Non-divisible Sums Difference/Solution.py
+++ 3172. Divisible and Non-divisible Sums Difference/Solution.py
@@ -2,9 +2,9 @@ class Solution:
     def differenceOfSums(self, n: int, m: int) -> int:
         not_divisible = []
         divisible = []
-        for i in range (1,n+1):
+        for i in range(1, n + 1):
             if i % m == 0:
                 divisible.append(i)
             else:
                 not_divisible.append(i)
-        return sum(not_divisible)-sum(divisible)
+        return sum(not_divisible) - sum(divisible)
