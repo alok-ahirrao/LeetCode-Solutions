4238a4e3971e43976a804c6ec621df4a03c1c055
Alok Ahirrao <104273243+alok-ahirrao@users.noreply.github.com>
2025-05-30 11:00:27 +0530

    Python3 Solution
          Submission Statistics:
          Question #: 2438
          Language: Python3
          Runtime: 33 ms
          Runtime percentile: 97.74
          Memory: 31.936 MB
          Memory percentile: 85.52

--- 2438. Find Closest Node to Given Two Nodes/Solution.py
+++ 2438. Find Closest Node to Given Two Nodes/Solution.py
@@ -1,23 +1,33 @@
 class Solution:
     def closestMeetingNode(self, edges: List[int], node1: int, node2: int) -> int:
-        n=len(edges)
-        inf=1<<32
-        dist1=[inf]*n
-        dist2=[inf]*n
-        def dfs(node, dist):
-            d, i=0, node
-            while i!=-1 and dist[i]==inf:
-                dist[i]=d
-                d+=1
-                i=edges[i]
-        dfs(node1, dist1)
-        dfs(node2, dist2)
-        minD, index=inf, -1
-        for i in range(n):
-            max12=max(dist1[i], dist2[i])
-            if max12<minD: 
-                minD=max12
-                index=i
-        return index
+        visited1 = set()
+        visited2 = set()
+        while node1 != -1 or node2 != -1:
+            if node1 in visited1:
+                node1 = -1
+            
+            if node2 in visited2:
+                node2 = -1
 
-        
+            if node1 != -1:
+                visited1.add(node1)
+            
+            if node2 != -1:
+                visited2.add(node2)
+
+            if node1 in visited2 and node2 in visited1:
+                return min(node1, node2)
+            
+            if node1 in visited2:
+                return node1
+            
+            if node2 in visited1:
+                return node2
+            
+            if node1 != -1:
+                node1 = edges[node1]
+
+            if node2 != -1:
+                node2 = edges[node2]       
+                 
+        return -1
