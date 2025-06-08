59f5ea9b6a8b55afad19a4d638779be864e06ff4
Alok Ahirrao <104273243+alok-ahirrao@users.noreply.github.com>
2025-06-08 12:42:11 +0530

    Python3 Solution
          Submission Statistics:
          Question #: 386
          Language: Python3
          Runtime: 8 ms
          Runtime percentile: 82.85
          Memory: 21.224 MB
          Memory percentile: 77.82

--- 386. Lexicographical Numbers/Solution.py
+++ 386. Lexicographical Numbers/Solution.py
@@ -1,3 +1,16 @@
 class Solution:
-    def lexicalOrder(self, n: int) -> List[int]:
-        return sorted(range(1, n+1), key=str)
+    def lexicalOrder(self, n: int) -> list[int]:
+        res = []
+        curr = 1
+        for _ in range(n):
+            res.append(curr)
+            if curr * 10 <= n:
+                curr *= 10
+            else:
+                # backtrack
+                if curr >= n:
+                    curr //= 10
+                curr += 1
+                while curr % 10 == 0:
+                    curr //= 10
+        return res
