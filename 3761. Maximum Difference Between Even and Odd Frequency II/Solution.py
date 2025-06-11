2a6d50f2a10c548df81cd8daaaf425a80f581059
Alok Ahirrao <104273243+alok-ahirrao@users.noreply.github.com>
2025-06-11 21:36:58 +0530

    Python3 Solution
          Submission Statistics:
          Question #: 3761
          Language: Python3
          Runtime: 845 ms
          Runtime percentile: 100.00
          Memory: 19.924 MB
          Memory percentile: 65.52

--- 3761. Maximum Difference Between Even and Odd Frequency II/Solution.py
+++ 3761. Maximum Difference Between Even and Odd Frequency II/Solution.py
@@ -1,39 +1,43 @@
 class Solution:
-    def maxDfromAtoB(self, a: int, b: int, k: int, n: int, freq: List[List[int]]) -> int:
-        cnt = float('-inf')
-        MOD = 10 ** 8
-        minFreq = [[MOD, MOD], [MOD, MOD]]
-        freqA = 0
-        freqB = 0
-        prevA = 0
-        prevB = 0
-        l = 0
-        for r in range(k - 1, n):
-            freqA = freq[a][r + 1]
-            freqB = freq[b][r + 1]
-            while r - l + 1 >= k and freqB - prevB >= 2:
-                minFreq[prevA & 1][prevB & 1] = min(minFreq[prevA & 1][prevB & 1], prevA - prevB)
-                prevA = freq[a][l + 1]
-                prevB = freq[b][l + 1]
-                l += 1
-            cnt = max(cnt, freqA - freqB - minFreq[1 - (freqA & 1)][freqB & 1])
-        return cnt
     def maxDifference(self, s: str, k: int) -> int:
+        idx = [[] for i in range(5)] # index of char
+        for i,c in enumerate(s):
+            idx[int(c)].append(i)
+        idx = [q for q in idx if len(q)] # remove empty
         n = len(s)
-        freq = [[0] * (n + 1) for _ in range(5)]
-        conter = Counter(s)
-        for i in range(n):
-            for d in range(5):
-                freq[d][i + 1] = freq[d][i]
-            freq[int(s[i])][i + 1] += 1
-        ans = float('-inf')
-        for a in range(5):
-            if freq[a][n] == 0:
-                continue
-            for b in range(5):
-                if a == b or freq[b][n] == 0:
-                    continue
-                ans = max(ans, self.maxDfromAtoB(a, b, k, n, freq))
+        for q in idx: q.append(n) #sentinel to simplify end case
+        def sol(q1,q2): # max (odd q1 - even q2)
+            # monotonic deque (alive time, val of q1-q2)
+            que = [deque() for i in range(4)] # (odd,even)*(odd,even)
+            prev = [inf]*4 # prefix alive minima 
+            que[0].append((max(q1[0],q2[0],k-1),0))
+            i=j=0; best = -inf
+            curr = -1 # current right position
+            # invariant: next q1 and next q2
+            while q1[i]!=q2[j]: # end when both end (sentinel)
+                if q1[i]<q2[j]: # next q1 enter 
+                    if q1[i]>curr: curr=q1[i]
+                    i += 1
+                else: # next q2 enter
+                    if q2[j]>curr: curr=q2[j]
+                    j += 1
+                # alive right position = before next coming
+                right = min(q1[i],q2[j])-1  
+                parity = (i&1)|((j&1)<<1) # prefix parity
+                prev_p = parity^1 # diff bit0, same bit 1
+                # wake up alive
+                while que[prev_p] and que[prev_p][0][0]<=right:
+                    _,prev[prev_p] = que[prev_p].popleft()
+                best = max(best, i-j - prev[prev_p])
+                # push (i-j)
+                v = i-j
+                # alive when curr+k and next i and next j
+                if v<prev[parity] and ((not que[parity]) or v<que[parity][-1][1]):
+                    que[parity].append((max(curr+k,q1[i],q2[j]),v))
+            # while
+            return best
+        # end sol
+        ans = -inf
+        for q1,q2 in permutations(idx,2):
+            ans = max(ans,sol(q1,q2))
         return ans
-
-        
