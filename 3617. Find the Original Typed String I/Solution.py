f7c4705e173bbf721c2f96f9306510c551f7282c
Alok Ahirrao <104273243+alok-ahirrao@users.noreply.github.com>
2025-07-01 16:56:36 +0530

    Python3 Solution
          Submission Statistics:
          Question #: 3617
          Language: Python3
          Runtime: 44 ms
          Runtime percentile: 24.23
          Memory: 17.612 MB
          Memory percentile: 72.11

---
+++ 3617. Find the Original Typed String I/Solution.py
@@ -0,0 +1,20 @@
+class Solution:
+    def possibleStringCount(self, word: str) -> int:
+
+        stack = []
+
+        ans = 1 
+
+        for w in word:
+            if not stack or (stack and w==stack[-1]):
+                stack.append(w)
+            else:
+                ans+=len(stack)-1
+                stack = [w]
+            
+            #print(w,stack,ans)
+
+        ans+=len(stack)-1
+        return ans 
+
+        
