6336fedf560d5b0778abff7e245b49cafab2ec9f
Alok Ahirrao <104273243+alok-ahirrao@users.noreply.github.com>
2025-07-03 09:34:43 +0530

    Python3 Solution
          Submission Statistics:
          Question #: 3600
          Language: Python3
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 17.904 MB
          Memory percentile: 17.19

--- 3600. Find the K-th Character in String Game I/Solution.py
+++ 3600. Find the K-th Character in String Game I/Solution.py
@@ -1,9 +1,17 @@
 class Solution:
     def kthCharacter(self, k: int) -> str:
-        sb = ['a']
-        while len(sb) < k:
-            size = len(sb)
-            for i in range(size):
-                next_char = chr(ord('a') + ((ord(sb[i]) - ord('a') + 1) % 26))
-                sb.append(next_char)
-        return sb[k - 1]
+        # string=["a"]
+        # while len(string)<=k:
+        #     next_string=[chr(((ord(c)-ord('a')+1)%26)+ord('a')) for c in string]
+        #     string+=next_string
+        # return string[k-1]
+
+        ans = 0
+        while k != 1:
+            t = k.bit_length() - 1
+            if (1 << t) == k:
+                t -= 1
+            k -= 1 << t
+            ans += 1
+        return chr(ord("a") + ans)
+        
