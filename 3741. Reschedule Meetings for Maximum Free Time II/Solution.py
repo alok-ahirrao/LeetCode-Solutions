1b879866453b774d60d893402167dbd14198271f
Alok Ahirrao <104273243+alok-ahirrao@users.noreply.github.com>
2025-07-10 11:32:04 +0530

    Python3 Solution
          Submission Statistics:
          Question #: 3741
          Language: Python3
          Runtime: 152 ms
          Runtime percentile: 95.95
          Memory: 38.612 MB
          Memory percentile: 63.01

--- 3741. Reschedule Meetings for Maximum Free Time II/Solution.py
+++ 3741. Reschedule Meetings for Maximum Free Time II/Solution.py
@@ -1,34 +1,91 @@
+fmax = lambda a, b: a if a > b else b
+
 class Solution:
     def maxFreeTime(self, eventTime: int, startTime: List[int], endTime: List[int]) -> int:
-        size = len(startTime)
-        gapsArr,left = [],0
-        for i in range(size):
-            gap = startTime[i] - left
-            gapsArr.append(gap)
-            left = endTime[i]
-        gapsArr.append(eventTime - endTime[-1])
-        maxGapPrefix,maxGapSuffix = [0 for i in range(size)],[0 for i in range(size)]
-        maxGapPrefix[0] = gapsArr[0]
-        maxGapSuffix[size-1] = gapsArr[-1]
+        
+        head = 0
+        space = []
 
-        # calculate left right max
-        for i in range(1,size):
-            maxGapPrefix[i]= max(maxGapPrefix[i-1],gapsArr[i])
-        for i in range(size-1,0,-1):
-            maxGapSuffix[i-1]= max(maxGapSuffix[i],gapsArr[i])
+        for s, e in zip(startTime, endTime):
+            space.append([0, s - head])
+            space.append([1, e - s])
+            head = e
+
+        space.append([0, eventTime - endTime[-1]])
+
+        func = lambda total, x: fmax(total, x[1] if x[0] == 0 else -1)
+        pre_max = list(accumulate(space, func, initial=-1))
+        suf_max = list(accumulate(space[::-1], func, initial=-1))[::-1]
         
-        ans = 0
-        for i in range(size):
-            curr = gapsArr[i]+gapsArr[i+1]
-            barSize = endTime[i] - startTime[i]
-            isValid = False
-            if(i-1 >= 0):
-                isValid = isValid or maxGapPrefix[i-1] >= barSize
-            if(i+1 < size):
-                isValid = isValid or maxGapSuffix[i+1] >= barSize
-            if(isValid):
-                curr += barSize
-            ans = max(ans, curr)
-        return ans
+        ans = -inf
+        for i, (t, v) in enumerate(space):
+            if t == 0:
+                continue
             
-        
+            if pre_max[i - 1] >= v or suf_max[i + 2] >= v:
+                ans = fmax(ans, space[i - 1][1] + space[i + 1][1] + v)
+            else:
+                ans = fmax(ans, space[i - 1][1] + space[i + 1][1])
+
+        return ans
+
+
+    def maxFreeTime(self, eventTime: int, startTime: List[int], endTime: List[int]) -> int:
+
+        startTime = [0] + startTime + [eventTime]
+        endTime = [0] + endTime + [eventTime]
+
+        size = len(startTime)
+        suf_max = [-1] * size
+        suf_max[-1] = eventTime - endTime[-1]
+
+        for i in range(size - 2, -1, -1):
+            suf_max[i] = fmax(suf_max[i + 1], startTime[i + 1] - endTime[i])
+
+        ans = pre_max = -1
+        for i in range(1, size - 1):
+            s, e = startTime[i], endTime[i]
+            
+            space = (s - endTime[i - 1]) + (startTime[i + 1] - e)
+            duration = e - s
+
+            if duration <= pre_max or duration <= suf_max[i + 1]:
+                ans = fmax(ans, space + duration)
+            else:
+                ans = fmax(ans, space)
+            pre_max = fmax(pre_max, s - endTime[i - 1])
+
+        return ans
+
+
+class Solution1:
+    def maxFreeTime(self, eventTime: int, startTime: List[int], endTime: List[int]) -> int:
+        n = len(startTime)
+
+        def get(i: int) -> int:
+            if i == 0:
+                return startTime[0]
+            if i == n:
+                return eventTime - endTime[n - 1]
+            return startTime[i] - endTime[i - 1]
+
+        a, b, c = 0, -1, -1
+        for i in range(1, n + 1):
+            sz = get(i)
+            if sz > get(a):
+                a, b, c = i, a, b
+            elif b < 0 or sz > get(b):
+                b, c = i, b
+            elif c < 0 or sz > get(c):
+                c = i
+
+        ans = 0
+        for i, (s, e) in enumerate(zip(startTime, endTime)):
+            sz = e - s
+            if i != a and i + 1 != a and sz <= get(a) or \
+               i != b and i + 1 != b and sz <= get(b) or \
+               sz <= get(c):
+                ans = fmax(ans, get(i) + sz + get(i + 1))
+            else:
+                ans = fmax(ans, get(i) + get(i + 1))
+        return ans
