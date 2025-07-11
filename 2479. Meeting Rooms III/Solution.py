61effd7661851476442482c7b2f5549b4bf665a6
Alok Ahirrao <104273243+alok-ahirrao@users.noreply.github.com>
2025-07-11 11:58:55 +0530

    Python3 Solution
          Submission Statistics:
          Question #: 2479
          Language: Python3
          Runtime: 153 ms
          Runtime percentile: 94.02
          Memory: 51.508 MB
          Memory percentile: 34.22

--- 2479. Meeting Rooms III/Solution.py
+++ 2479. Meeting Rooms III/Solution.py
@@ -1,18 +1,19 @@
 class Solution:
-    def mostBooked(self, n: int, m: List[List[int]]) -> int:
-        r, c = [0]*n, [0]*n # rooms, counter
-        for s, e in sorted(m):
-            found = 0
-            for i,f in enumerate(r):
-                if f <= s:
-                    r[i] = e
-                    c[i] += 1
-                    found = 1
-                    break
+    def mostBooked(self, n: int, meetings: List[List[int]]) -> int:
+        counts, rooms, t = [0]*n, list(range(n)), 0
+        meetings, ongoingMeetings = sorted(meetings, key = lambda x: x[0]), []
+        heapq.heapify(rooms)
 
-            if not found:
-                q = r.index(min(r))
-                r[q] += e-s
-                c[q] += 1
+        for start, end in meetings:
+            while ongoingMeetings and ongoingMeetings[0][0]<=start:
+                heapq.heappush(rooms, heapq.heappop(ongoingMeetings)[1])
+            if rooms:
+                room = heapq.heappop(rooms)
+            else:
+                t, room = heapq.heappop(ongoingMeetings)
+                end += t-start
 
-        return c.index(max(c))
+            counts[room] += 1
+            heapq.heappush(ongoingMeetings, (end, room))
+            
+        return max(range(n), key = lambda x: (counts[x], -x))
