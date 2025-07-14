a1460d7272a299e4badc51e618d9a74b3664b2f8
Alok Ahirrao <104273243+alok-ahirrao@users.noreply.github.com>
2025-07-14 11:39:59 +0530

    Python3 Solution
          Submission Statistics:
          Question #: 1411
          Language: Python3
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 17.944 MB
          Memory percentile: 26.17

--- 1411. Convert Binary Number in a Linked List to Integer/Solution.py
+++ 1411. Convert Binary Number in a Linked List to Integer/Solution.py
@@ -5,10 +5,22 @@
 #         self.next = next
 class Solution:
     def getDecimalValue(self, head: Optional[ListNode]) -> int:
-        ans, curr=0, head
+        curr = head
+        summ = 0
+        list1 = []
         while curr:
-            ans=(ans<<1)+curr.val
-            curr=curr.next
-        return ans
+            list1.append(curr.val)
+            curr= curr.next
+
+        list1 = list1[::-1]
+        
+        for i in range(len(list1)):
+            print(i)
+            print(list1[i])
+            if list1[i] ==1 :
+                summ += 2 ** i
+
+        return summ
+
 
         
