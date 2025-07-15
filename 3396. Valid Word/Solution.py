89a1d598ccac7145249b815ecdb1b2b31c964ba1
Alok Ahirrao <104273243+alok-ahirrao@users.noreply.github.com>
2025-07-16 03:05:18 +0530

    Python3 Solution
          Submission Statistics:
          Question #: 3396
          Language: Python3
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 17.948 MB
          Memory percentile: 9.81

--- 3396. Valid Word/Solution.py
+++ 3396. Valid Word/Solution.py
@@ -1,13 +1,28 @@
-import re
-
 class Solution:
     def isValid(self, word: str) -> bool:
+
         if len(word) < 3:
             return False
-        if not re.fullmatch(r'[A-Za-z0-9]+', word):
-            return False
-        if not re.search(r'[aeiouAEIOU]', word):
-            return False
-        if not re.search(r'(?i)[b-df-hj-np-tv-z]', word):  
-            return False
-        return True
+        #if not word.isalnum():
+        #    return False
+
+
+        vowels = set("aeuoiAEUOI")
+        digits = set("0123456789")
+        consonants = set("bcdfghjklmnpqrstvwxyzBCDEFGHJKLMNPQRSTVWXYZ")
+
+        has_vowel = False
+        has_consonant = False
+
+        for letter in word:
+            if letter in vowels:
+                has_vowel = True
+            elif letter in consonants:
+                has_consonant = True
+            elif letter not in digits:
+                return False
+        #print(has_vowel, has_consonant)
+
+        return has_vowel and has_consonant
+
+        
