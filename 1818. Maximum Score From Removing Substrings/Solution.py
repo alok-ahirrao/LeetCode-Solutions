6e3f5ca25058d71816064af6ed047a63a2d0291b
Alok Ahirrao <104273243+alok-ahirrao@users.noreply.github.com>
2025-07-23 16:52:10 +0530

    Python3 Solution
          Submission Statistics:
          Question #: 1818
          Language: Python3
          Runtime: 132 ms
          Runtime percentile: 98.71
          Memory: 17.912 MB
          Memory percentile: 98.71

--- 1818. Maximum Score From Removing Substrings/Solution.py
+++ 1818. Maximum Score From Removing Substrings/Solution.py
@@ -1,22 +1,23 @@
 class Solution:
     def maximumGain(self, s: str, x: int, y: int) -> int:
-        
-        def solve(text: str, pattern: str, score: int):
-            stack = []
-            points = 0
-            for char in text:
-                if stack and stack[-1] == pattern[0] and char == pattern[1]:
-                    stack.pop()
-                    points += score
-                else:
-                    stack.append(char)
-            return points, "".join(stack)
-
-        if x >= y:
-            points1, remaining_s = solve(s, "ab", x)
-            points2, _ = solve(remaining_s, "ba", y)
+        if x > y:
+            return self.getMax(s, 'a', 'b', x, y)
         else:
-            points1, remaining_s = solve(s, "ba", y)
-            points2, _ = solve(remaining_s, "ab", x)
-            
-        return points1 + points2
+            return self.getMax(s, 'b', 'a', y, x)
+        
+    def getMax(self, s: str, a:str, b:str, x: int, y: int):
+        c1 = c2 = ans = 0
+        s += 'c'
+        for c in s:
+            if c == a:
+                c1 += 1
+            elif c == b:
+                if c1 == 0:
+                    c2 += 1
+                else:
+                    ans += x
+                    c1 -= 1
+            else:
+                ans += y * min(c1, c2)
+                c1 = c2 = 0
+        return ans
