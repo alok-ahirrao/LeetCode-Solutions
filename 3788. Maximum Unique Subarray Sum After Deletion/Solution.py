51310dc0223a46f52034beaf27a2b30ff5a98365
Alok Ahirrao <104273243+alok-ahirrao@users.noreply.github.com>
2025-07-25 11:25:00 +0530

    Python3 Solution
          Submission Statistics:
          Question #: 3788
          Language: Python3
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 17.656 MB
          Memory percentile: 85.25

--- 3788. Maximum Unique Subarray Sum After Deletion/Solution.py
+++ 3788. Maximum Unique Subarray Sum After Deletion/Solution.py
@@ -1,9 +1,11 @@
 class Solution:
     def maxSum(self, nums: List[int]) -> int:
-        s=0
-        for i in set(nums) :
-            if i >0:
-                s+=i
-        if s==0:
+        if max(nums)<0 :
             return max(nums)
-        return s
+        else:
+            result=0
+            nums=set(nums)
+            for i in nums:
+                if i>=0:
+                    result+=i
+            return result
