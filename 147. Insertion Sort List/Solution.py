6db5d6a8861601a33aa454672320d3f054d7ed36
Alok Ahirrao <104273243+alok-ahirrao@users.noreply.github.com>
2025-08-06 12:40:21 +0530

    Python3 Solution
          Submission Statistics:
          Question #: 147
          Language: Python3
          Runtime: 0 ms
          Runtime percentile: 100.00
          Memory: 18.852 MB
          Memory percentile: 43.76

--- 147. Insertion Sort List/Solution.py
+++ 147. Insertion Sort List/Solution.py
@@ -1,28 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 class Solution:
-    def insertionSortList(self, head: ListNode) -> ListNode:
+    def insertionSortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        l=[]
+        h=head
+        while h:
+            l.append(h.val)
+            h=h.next 
+        h=head
+        l.sort()
+        i=0
+        while h:
+            h.val=l[i]
+            i+=1 
+            h=h.next 
+        return head
+
         
-        # No need to sort for empty list or list of size 1
-        if not head or not head.next:
-            return head
-        
-        # Use dummy_head will help us to handle insertion before head easily
-        dummy_head = ListNode(val=-5000, next=head)
-        last_sorted = head # last node of the sorted part
-        cur = head.next # cur is always the next node of last_sorted
-        while cur:
-            if cur.val >= last_sorted.val:
-                last_sorted = last_sorted.next
-            else:
-                # Search for the position to insert
-                prev = dummy_head
-                while prev.next.val <= cur.val:
-                    prev = prev.next
-                    
-                # Insert
-                last_sorted.next = cur.next
-                cur.next = prev.next
-                prev.next = cur
-                
-            cur = last_sorted.next
-            
-        return dummy_head.next
