f55c53f7123aa6cdc144a189a0c544d331f422dc
Alok Ahirrao <104273243+alok-ahirrao@users.noreply.github.com>
2025-08-19 10:26:49 +0530

    Python3 Solution
          Submission Statistics:
          Question #: 2432
          Language: Python3
          Runtime: 24 ms
          Runtime percentile: 94.37
          Memory: 28.396 MB
          Memory percentile: 83.40

--- 2432. Number of Zero-Filled Subarrays/Solution.py
+++ 2432. Number of Zero-Filled Subarrays/Solution.py
@@ -1,13 +1,22 @@
 class Solution:
-    def zeroFilledSubarray(self, nums):
-        ans = 0
-        n = len(nums)
-        curr = 0
-        for i in range(n):
-            if nums[i] == 0:
-                curr += 1
+    def zeroFilledSubarray(self, nums: List[int]) -> int:
+        length_of_zero = 0
+        total = 0
+
+        for num in nums:
+
+            if(num == 0):
+                length_of_zero += 1
             else:
-                ans += curr * (curr + 1) // 2
-                curr = 0
-        ans += curr * (curr + 1) // 2
-        return ans
+                if(length_of_zero != 0):
+                    total += (length_of_zero * (length_of_zero + 1)) // 2
+                    length_of_zero = 0
+        
+        if(length_of_zero != 0):
+            total += (length_of_zero * (length_of_zero + 1)) // 2
+        
+        return total
+
+
+
+        
