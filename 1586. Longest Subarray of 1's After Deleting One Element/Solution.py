af14720b0ccbd6e4a6d575dd70eddfc69db06551
Alok Ahirrao <104273243+alok-ahirrao@users.noreply.github.com>
2025-08-24 07:21:57 +0530

    Python3 Solution
          Submission Statistics:
          Question #: 1586
          Language: Python3
          Runtime: 11 ms
          Runtime percentile: 99.12
          Memory: 21.948 MB
          Memory percentile: 6.20

--- 1586. Longest Subarray of 1's After Deleting One Element/Solution.py
+++ 1586. Longest Subarray of 1's After Deleting One Element/Solution.py
@@ -1,12 +1,30 @@
 class Solution:
     def longestSubarray(self, nums: List[int]) -> int:
-        n, l, zeros, ans=len(nums), 0, 0, 0
-        for r, x in enumerate(nums):
-            zeros+=(x==0)
-            if zeros>1:
-                zeros-=(nums[l]==0)
-                l+=1
-            ans=max(ans, r-l)
-        return ans
-            
+        if sum(nums) == len(nums):
+            return(len(nums)-1)
+        
+        if sum(nums) == 0:
+            return(0)
+        
+        locs = []
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                locs.append(i)
+        
+        if len(locs) == 1:
+            return(len(nums)-1)
+        
+        size = locs[1] - 0 - 1
+        max_size = size
+
+        if len(locs) > 2:
+            for i in range(2, len(locs)):
+                size = locs[i] - locs[i-2] - 1 - 1
+                max_size = max(size, max_size)
+        
+        if locs[-1] < len(nums)-1:
+            size = len(nums)-1 - locs[-2] - 1
+            max_size = max(size, max_size)
+        
+        return(max_size)
         
