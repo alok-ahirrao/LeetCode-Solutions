e208d2114f25c5f4e6db5690029dba22e11b922d
Alok Ahirrao <104273243+alok-ahirrao@users.noreply.github.com>
2025-09-02 10:47:16 +0530

    Python3 Solution
          Submission Statistics:
          Question #: 3278
          Language: Python3
          Runtime: 3 ms
          Runtime percentile: 100.00
          Memory: 17.96 MB
          Memory percentile: 20.51

--- 3278. Find the Number of Ways to Place People I/Solution.py
+++ 3278. Find the Number of Ways to Place People I/Solution.py
@@ -1,11 +1,13 @@
 class Solution:
-    def numberOfPairs(self, P: List[List[int]]) -> int:
-        P.sort(key=lambda p: (-p[0], p[1]))
-        n, ans=len(P), 0
-        for i in range(n-1):
-            y=1<<31
-            for j in range(i+1, n):
-                if y>P[j][1]>=P[i][1]:
-                    ans+=1
-                    y=P[j][1]
-        return ans
+    def numberOfPairs(self, points: List[List[int]]) -> int:
+        points.sort(key=lambda x: (x[0], -x[1]))
+        result = 0
+        for i, (_, top) in enumerate(points):
+            bot = -inf
+            for (_, y) in points[i + 1:]:
+                if bot < y <= top:
+                    result += 1
+                    bot = y
+                    if top == bot:
+                        break
+        return result
