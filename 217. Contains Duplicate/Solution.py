686d26abbdedeb9737c418efc951cd746aa48afd
Alok Ahirrao <104273243+alok-ahirrao@users.noreply.github.com>
2025-09-10 21:37:33 +0530

    Python3 Solution
        Submission Statistics:
        Question #: 217
        Language: Python3
        Runtime: 19 ms
        Runtime percentile: 32.74
        Memory: 31.388 MB
        Memory percentile: 90.61

--- 217. Contains Duplicate/Solution.py
+++ 217. Contains Duplicate/Solution.py
@@ -1,9 +1,3 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        hash_set = set()
-        for i in nums:
-            if i in hash_set:
-                return True
-            
-            hash_set.add(i)
-        return False
+        return len(nums)!=len(set(nums))
